567e2561692ab11dc875082a966e0bab71135abb
x <chenglong.zq@gmail.com>
2018-03-05 07:37:38 +0100

    Update reporter specs

--- pipeline/reporter.py
+++ pipeline/reporter.py
@@ -20,25 +20,153 @@ class ReportGenerator(object):
         pass
 
     def parse_err_compile_source(self):
+        """
+        Kayo
+        ---------------------------------------------------------------------
+        src/Person.java:1: error: '.' expected
+        import magic
+                     ^
+        src/Person.java:3: error: ';' expected
+        public class Person {
+              ^
+        src/Person.java:3: error: class, interface, or enum expected
+        public class Person {
+                     ^
+        src/Person.java:6: error: class, interface, or enum expected
+          public Person(String personName) {
+                 ^
+        src/Person.java:8: error: class, interface, or enum expected
+          }
+          ^
+        src/Person.java:10: error: class, interface, or enum expected
+          public String greet(String yourName) {
+                 ^
+        src/Person.java:13: error: class, interface, or enum expected
+                  return String.format("Konbanwa!  My name is %s.  It is nice to meet you, %s!", this.name, yourName);
+                  ^
+        src/Person.java:14: error: class, interface, or enum expected
+          }
+          ^
+        8 errors
+        ---------------------------------------------------------------------
+
+        :return:
+
+        {
+        "err_compile_source" : [
+            {
+            "line": "1"
+            "column": "\^\^ length"
+            "file": "src/Person.java",
+            "message": "error: '.' expected",
+            "source": "import obytes"
+            },
+            {
+            etcetcetc
+            }
+        ]
+        }
+
+        """
         raise NotImplementedError
         pass
 
     def parse_err_compile_test(self):
-        raise NotImplementedError
+        """
+        -------------------------------
+        OKey
+        -------------------------------
+
+        -------------------------------
+        Kayo
+        -------------------------------
+
+        -------------------------------
+
+        :return:
+        """
+        #raise NotImplementedError
         pass
 
     def parse_err_run_test(self):
-        raise NotImplementedError
+        """
+        -------------------------------
+        OKey
+        -------------------------------
+
+        -------------------------------
+        Kayo
+        -------------------------------
+
+        -------------------------------
+
+        :return:
+        """
+        # raise NotImplementedError
+        # no demo source provided
         pass
 
     def parse_out_compile_source(self):
-        raise NotImplementedError
+        """
+        -------------------------------
+        OKey
+        -------------------------------
+
+        -------------------------------
+        Kayo
+        -------------------------------
+
+        -------------------------------
+
+        :return:
+        """
+        #raise NotImplementedError
         pass
 
     def parse_out_compile_test(self):
-        raise NotImplementedError
+        """
+        -------------------------------
+        OKey
+        -------------------------------
+
+        -------------------------------
+        Kayo
+        -------------------------------
+
+        -------------------------------
+
+        :return:
+        """
+        # raise NotImplementedError
         pass
 
     def parse_out_run_test(self):
-        raise NotImplementedError
+        """
+        -------------------------------
+        OKey
+        -------------------------------
+        JUnit version 4.12
+        .
+        Time: 0.003
+
+        OK (1 test)
+        -------------------------------
+        Kayo
+        -------------------------------
+
+        -------------------------------
+
+
+        :return:
+        {
+        "out_run_test":
+            {
+            "framework-version": "JUnit version 4.12"
+            "time": "0.003"
+            "result": "OK"
+            "summary": "(1 test)"
+            }
+        }
+        """
+        #raise NotImplementedError
         pass
